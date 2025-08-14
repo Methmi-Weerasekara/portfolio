ba616dccb2258b4394cadaabde45877a6aaa6b25
Methmi-Weerasekara <methkaviwee@gmail.com>
2025-08-14 12:24:52 +0530

    Adding menubar javascript

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contactForm");
   const response = document.getElementById("formResponse");
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault(); 
+  if (form && response) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+         response.style.display = "block";
+    });
+  }
 
-     response.style.display = "block";
+  const menuIcon = document.getElementById("menu-icon");
+  const navLinks = document.querySelector(".nav-links");
 
-  });
+  if (menuIcon && navLinks) {
+       menuIcon.addEventListener("click", function () {
+      navLinks.classList.toggle("active");
+    });
+  }
 });
