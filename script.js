8adb33985f6218cfdde3a8a9d31431649528807e
Methmi-Weerasekara <methkaviwee@gmail.com>
2025-08-14 13:01:17 +0530

    Correcting the js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menu-icon");
   const navLinks = document.querySelector(".nav-links");
 
-  if (menuIcon && navLinks) {
-       menuIcon.addEventListener("click", function () {
-      navLinks.classList.toggle("active");
-    });
-  }
+  menuIcon.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
 });
